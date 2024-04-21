315abc7f8ce1caa307d186ed9403b6580cd02f97
Yeonsu Jung <ynsuju@gmail.com>
2024-04-21 15:49:06 -0400

    Create visualizations.py

---
+++ visualizations.py
@@ -0,0 +1,62 @@
+import jax.numpy as jnp
+from jax import grad, jit, vmap
+from jax import random
+from jax import lax
+from optimization import optimize_fire2,optimize_fire_debug
+from potentials import effective_potential,total_effective_potential,total_effective_potential_ref
+import numpy as onp
+from matplotlib import pyplot as plt
+import time
+
+def plot_many_rods(q):
+    N = q.shape[0]
+    for i in range(N):        
+        plot_rod(q[i,:])
+        
+    return 1
+def plot_rod(q_single):
+    q_onp = onp.array(q_single)
+    x1 = q_onp[0]
+    y1 = q_onp[1]
+    z1 = q_onp[2]
+    phi1 = q_onp[3]
+    theta1 = q_onp[4]
+    rod_length = 1.
+
+    x11 = x1 + rod_length*jnp.sin(phi1)*jnp.cos(theta1)
+    y11 = y1 + rod_length*jnp.sin(phi1)*jnp.sin(theta1)
+    z11 = z1 + rod_length*jnp.cos(phi1)
+    plt.plot([x1, x11], [y1, y11], [z1, z11])
+    
+def plot_rods(q):
+    q_onp = onp.array(q)
+    x1 = q_onp[0]
+    y1 = q_onp[1]
+    z1 = q_onp[2]
+    phi1 = q_onp[3]
+    theta1 = q_onp[4]
+    rod_length = 1.
+
+    x11 = x1 + rod_length*jnp.sin(phi1)*jnp.cos(theta1)
+    y11 = y1 + rod_length*jnp.sin(phi1)*jnp.sin(theta1)
+    z11 = z1 + rod_length*jnp.cos(phi1)
+
+    x2 = q_onp[5]
+    y2 = q_onp[6]
+    z2 = q_onp[7]
+    phi2 = q_onp[8]
+    theta2 = q_onp[9]
+
+    x22 = x2 + rod_length*jnp.sin(phi2)*jnp.cos(theta2)
+    y22 = y2 + rod_length*jnp.sin(phi2)*jnp.sin(theta2)
+    z22 = z2 + rod_length*jnp.cos(phi2)
+
+    # 3d plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.plot([x1, x11], [y1, y11], [z1, z11])
+    ax.plot([x2, x22], [y2, y22], [z2, z22])
+# (q0, f, df, params, atol=1e-4, dt=0.002, logoutput=False, Nmax=10000):
