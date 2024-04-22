846b2f9b8bf4fb4101044fd9930d5803dcad1173
Yeonsu Jung <ynsuju@gmail.com>
2024-04-22 03:43:38 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,7 +3,9 @@ import jax.numpy as jnp
 import jax
 from potentials import create_pairs, all_pairwise_distances
 from matplotlib import pyplot as plt
+
 from visualizations import plot_many_rods, plot_contacts, set_3d_plot
+from data_io import read_data
 
 jax.config.update("jax_enable_x64", True)
 
@@ -26,11 +28,12 @@ def find_contacts(q,rod_radius):
         
     contact_degrees = np.zeros(num_rods)
     neighbors = []
-    # make it faster?
+    
+    # TO DO: make it faster?
     for i in range(num_rods):
         # nnz
-        contact_degrees[i] = np.count_nonzero(contacts[:,0] == i) + np.count_nonzero(contacts[:,1] == i)
-        neighbors.append(np.concatenate([contacts[contacts[:,0] == i,1], contacts[contacts[:,1] == i,0]]))
+        contact_degrees[i] = jnp.count_nonzero(contacts[:,0] == i) + jnp.count_nonzero(contacts[:,1] == i)
+        neighbors.append(jnp.concatenate([contacts[contacts[:,0] == i,1], contacts[contacts[:,1] == i,0]]))
     
     print('Number of contacts: ', num_contacts)
     print('Average number of contacts per rod: ', avg_num_contacts_per_rod)
@@ -42,15 +45,10 @@ def find_contacts(q,rod_radius):
     return 1
 
     
-def read_data(pth):
-    q = np.loadtxt(pth)
-    q = jnp.array(q,dtype=jnp.float64)
-    return q
 
 if __name__ == '__main__':
     pth = '/Users/yeonsu/Data/entangled_rods_N300_relaxed_22-04-2024_00-36-18.txt'
     q = read_data(pth)    
-    rod_radius = 0.08 # TO DO: read from file
-    
+    rod_radius = 0.08 # TO DO: read from file    
     contacts = find_contacts(q,rod_radius)
     
