65847ca97b366b6e36cfe0967b56a50cb5077513
Yeonsu Jung <ynsuju@gmail.com>
2024-05-07 15:09:36 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 from numpy import loadtxt, savetxt
 import numpy as np
 from jax import numpy as jnp
+from datetime import datetime
+import os
+import glob
+import shutil
 
 def timeit(func):
     import time
@@ -16,6 +20,26 @@ def parse_id_string(filename):
     # remove .txt extension, and join the rest
     filepart = '.'.join(filepart.split('.')[:-1])    
     return filepart.split('_')
+
+def archiving(folder_name=None):
+    # dt string in YYYY-MM-DD_HH-MM-SS
+    dt_string = datetime.now().strftime("%Y%m%d-%H%M%S")
+    if folder_name is None:
+        folder_name = f"/Users/yeonsu/Data/cache/{dt_string}"
+    else:
+        folder_name = f"{folder_name}/{dt_string}"
+    # hash = hashlib.md5(dt_string.encode()).hexdigest()
+    
+    os.makedirs(folder_name, exist_ok=False)
+    
+    source_dir = './'    
+    # copy every py file to the folder
+    files = glob.iglob(os.path.join(source_dir, "*.py"))
+    for file in files:
+        if os.path.isfile(file):
+            shutil.copy2(file, folder_name)
+    
+    return dt_string, folder_name
     
 if __name__ == '__main__':
     
