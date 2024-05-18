e7d17049c0e47d661e39cc52cc6eb2446614919d
Yeonsu Jung <ynsuju@gmail.com>
2024-05-18 13:25:05 -0400

    Update scripting_download.py

--- scripting_download.py
+++ scripting_download.py
@@ -1,35 +1,26 @@
 import os
 
-
 pathlist = []
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1614_RUN_Flynn,N300_AR300_mu0.5_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR300_mu0.4_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR300_mu0.3_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR300_mu0.2_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR200_mu0.5_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR200_mu0.3_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR200_mu0.4_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR125_mu0.5_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR200_mu0.2_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR125_mu0.4_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR125_mu0.2_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR125_mu0.3_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR100_mu0.5_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR100_mu0.4_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR100_mu0.3_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR100_mu0.2_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR75_mu0.5_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR75_mu0.4_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR75_mu0.3_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR75_mu0.2_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR50_mu0.4_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR50_mu0.5_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR20_mu0.4_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR50_mu0.3_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR20_mu0.3_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR20_mu0.2_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR50_mu0.2_visc0.0_amp1.0')
-pathlist.append('/n/home01/yjung/Github/dismech-rods-main/runs/20240507-1613_RUN_Flynn,N300_AR20_mu0.5_visc0.0_amp1.0')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha38_epsilon10')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha38_epsilon05')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha200_epsilon10')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha200_epsilon15')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha200_epsilon05')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha38_epsilon00')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha200_epsilon00')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha38_epsilon15')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha100_epsilon15')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha100_epsilon10')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha100_epsilon05')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha100_epsilon00')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha66_epsilon10')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha66_epsilon15')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha66_epsilon05')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha66_epsilon00')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha76_epsilon15')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha76_epsilon10')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha76_epsilon05')
+pathlist.append('/n/home01/yjung/good-bye-matlab/steel-rods-xray-data/alpha76_epsilon00')
 
 with open('downloaded_list.txt','a') as f:
     for pth in pathlist:
@@ -41,22 +32,14 @@ with open('download.sh', 'w') as f:
     f.write(f"#!/bin/bash\n")
     f.write("sftp sftp://yjung@odyssey.rc.fas.harvard.edu <<EOT\n")
 
-    for pth in pathlist:
-            
-        batch_info = pth.split('/')[-1].split(',')[0]
-        date_time = batch_info.split('_')[0]
-        batch_id = batch_info.split('_')[-1]
-        
-        sim_params_string = pth.split('/')[-1].split(',')[-1]
-        datalist.append({'date_time': date_time, 'batch_id': batch_id, 'sim_params_string': sim_params_string})        
-        
-        data_root = "/Users/yeonsu/Data"
-        local_path = f"{data_root}/{batch_id},/{date_time}/{sim_params_string}"
-        
+    for pth in pathlist:        
+        folder_name = pth.split('/')[-1]
+        data_root = "/Users/yeonsu/Data/steel-rods-xray-data"
+        local_path = f"{data_root}/{folder_name}"
         print(local_path)
+
+        f.write(f"get {pth}/adjacency_distance_scale0p98_threshold0p3_ij_score.pkl {local_path}/.\n")
         
-        os.makedirs(local_path, exist_ok=True)
-        f.write(f"get -r {pth}/log_files/*.csv {local_path}/.\n")
         
     f.write('quit\n')
     f.write('EOT\n')
