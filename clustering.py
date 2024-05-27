894d4634d66440796b62abcd2a4d04c70281eca0
Yeonsu Jung <ynsuju@gmail.com>
2024-05-27 03:25:03 -0400

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,5 +1,5 @@
-from scipy.spatial import distance
-from scipy.cluster import hierarchy
+# from scipy.spatial import distance
+# from scipy.cluster import hierarchy
 import numpy as np
 from pathlib import Path
 import networkx as nx
@@ -9,15 +9,143 @@ from data_io import load_xray_data
 from visualizations import set_3d_plot, plot_single_rod, plot_centerline_with_container
 from matplotlib import pyplot as plt
 from sklearn.cluster import DBSCAN
-from example_PhysicalRodRelaxation import prep_svd_cylinder,lumelsky_dist_vec, lumelsky_dist
 from numba import jit, prange
 from scipy.io import loadmat
-
 from potentials import dist_lin_seg_nonjax
-
-
 from sklearn.cluster import KMeans
 from sklearn_extra.cluster import KMedoids
+from distances import lumelsky_dist_vec
+
+
+def minimum_spanning_forest(graph):
+    msf = nx.Graph()
+    for component in nx.connected_components(graph):
+        subgraph = graph.subgraph(component)
+        mst = nx.minimum_spanning_tree(subgraph, weight='weight')
+        msf = nx.compose(msf, mst)
+    return msf
+
+def find_edges_to_remove_by_weight(a_graph):
+    min_weight_edges = {}
+    
+    for node in a_graph.nodes():
+        edges = list(a_graph.edges(node, data='weight'))
+        if edges:
+            # Find the two minimum weight edges for each node
+            sorted_edges = sorted(edges, key=lambda x: x[2])
+            min_edges = sorted_edges[:2]  # Take the minimum and next minimum edges
+
+            # Store these edges ensuring each node is only considered once per edge
+            for edge in min_edges:
+                node1, node2, weight = edge
+                if node1 not in min_weight_edges:
+                    min_weight_edges[node1] = []
+                if node2 not in min_weight_edges:
+                    min_weight_edges[node2] = []
+                if len(min_weight_edges[node1]) < 2:
+                    min_weight_edges[node1].append(edge)
+                if len(min_weight_edges[node2]) < 2:
+                    min_weight_edges[node2].append(edge)
+
+    # Collect edges to keep
+    edges_to_keep = {tuple(edge[:2]) for edges in min_weight_edges.values() for edge in edges}
+
+    # Collect edges to remove
+    edges_to_remove = set(a_graph.edges()) - edges_to_keep
+
+    return edges_to_remove
+
+
+
+def get_local_dist_mat(svd_cylinders,a_cluster,segments):
+
+    
+    num_elements = len(a_cluster)
+    local_dist_mat = np.full((num_elements,num_elements),np.inf)    
+    for i in range(num_elements):
+        cylinder_i = svd_cylinders[a_cluster[i],:]
+        segment_i = segments[a_cluster[i]]
+        p1,p2 = cylinder_i[0:3],cylinder_i[3:6]
+        
+        fit_result1 = fit_rod(segment_i,0.000001,10000)
+        slopes1 = fit_result1['slopes']
+        
+        for j in range(i+1,num_elements):
+            cylinder_j = svd_cylinders[a_cluster[j],:]
+            segment_j = segments[a_cluster[j]]
+            q1,q2 = cylinder_j[0:3],cylinder_j[3:6]
+            
+            t,u,d1,d2,d12=lumelsky_dist_vec(p1,p2,q1,q2)
+            
+            fit_result2 = fit_rod(segment_j,0.000001,10000)
+            slopes2 = fit_result2['slopes']
+            
+            joined = np.vstack([segment_i,segment_j])
+            fit_result = fit_rod(joined,0.000001,10000)
+            err_factor = fit_result['err']
+            
+            vec = d1*t - d2*u - d12
+            popt1 = p1+t*d1
+            popt1_opp = p1+(1-t)*d1
+            
+            popt2 = q1+u*d2
+            popt2_opp = q2+(1-u)*d2
+            
+            if t > 0.5:
+                slope1 = slopes1[-1]
+            else:
+                slope1 = slopes1[0]
+                
+            if u > 0.5:
+                slope2 = slopes2[-1]
+            else:
+                slope2 = slopes2[0]
+                
+            slope1 = slope1 * np.sign( np.dot(slope1,popt1 - popt1_opp) )
+            slope2 = slope2 * np.sign( np.dot(slope2,popt2 - popt2_opp) )
+            
+            dist_vec_sign = -np.sign(np.dot(slope1,slope2))
+            dist_vec_sign = 1
+            dist = np.linalg.norm(vec)
+            local_dist_mat[i,j] = dist*dist_vec_sign*err_factor
+            
+    local_dist_mat = np.minimum(local_dist_mat,local_dist_mat.T)
+    local_dist_mat[local_dist_mat<0] = 1e4
+    local_dist_mat[local_dist_mat == np.inf] = 1e4
+    return local_dist_mat
+
+
+def explode_local_cluster(a_cluster,svd_cylinders,segments):
+    subgraph = nx.Graph()
+    subgraph.add_nodes_from(range(len(a_cluster)))
+    # get_local_dist_mat(svd_cylinders,a_cluster,segments)
+    local_dist_mat = get_local_dist_mat(svd_cylinders,a_cluster,segments)
+    local_i,local_j = np.triu_indices(len(a_cluster),1)
+
+    subgraph.remove_edges_from(subgraph.edges())
+    subgraph.add_weighted_edges_from(zip(local_i,local_j,local_dist_mat[np.triu_indices(len(a_cluster),1)]))
+    # first make sure a node has two edges
+    edges_to_remove = find_edges_to_remove_by_weight(subgraph)
+    subgraph.remove_edges_from(edges_to_remove)
+    # minimum spanning tree
+    mst = nx.minimum_spanning_tree(subgraph, weight='weight',algorithm='boruvka')
+    mst_deg = np.array(list(mst.degree()))[:,1]
+
+    # remove hubs
+    hubs = np.where(mst_deg > 2)[0]
+    mst.remove_nodes_from(hubs)
+    subclusters = find_connected_components(mst)
+    
+    for i,subcluster in enumerate(subclusters):
+        subclusters[i] = np.sort(np.array(list(subcluster)).astype(int))
+    
+    return subclusters
+
+def find_connected_components(a_graph):
+        clusters = list(nx.connected_components(a_graph))
+        for i,cluster in enumerate(clusters):
+            clusters[i] = np.sort(np.array(list(cluster)).astype(int))        
+        return clusters
 
 def local_clustering_by_kmedoids(segments,a_cluster,N_rods_estimate,align_connectivity):
     fitting_score_over_trials = np.full(N_rods_estimate+1,np.inf)
@@ -848,7 +976,7 @@ def adaptive_staircase(segments,svd_cylinders,ijs,align_score,dist_score,segment
                 
             cluster_size_list = [len(x) for x in connected_component_list]
             max_cluster_size = np.max(cluster_size_list)
-            print(f'Max cluster size: {max_cluster_size}')
+            # print(f'Max cluster size: {max_cluster_size}')
         
             if max_cluster_size > max_cluster_size_criterion:
                 continue
@@ -1110,6 +1238,108 @@ def do_what():
     # plot_single_rod(np.vstack([segments[j] for j in connected_component_list[i]]),'.',ax=ax,markersize=0.2)
     # plot_single_rod(np.vstack([segments[cc[j]] for j in deg0]),'o',ax=ax,markersize=1)
     # ax.axis('equal')
+def subclustering_by_mst(clusters,segments,svd_cylinders,subcluster_error_threshold,subcluster_length_threshold,subcluster_length_tolerance=50):
+    N_segments = len(segments)    
+    segments_length_list = np.zeros(N_segments)
+    for i,seg in enumerate(segments):
+        segments_length_list[i] = np.sum(np.sqrt(np.sum(np.diff(seg,axis=0)**2,axis=1)))
+    cluster_length_list = np.zeros(len(clusters))
+    for i in range(len(clusters)):
+        cluster_length_list[i] = segments_length_list[clusters[i]].sum()
+    # np.argsort(cluster_length_list)[::-1]
+
+    good_clusters = []
+    for i_cluster, a_cluster in enumerate(clusters):
+        subclusters = explode_local_cluster(a_cluster,svd_cylinders,segments)
+        # check quality
+        subcluster_error_list = np.zeros(len(subclusters))
+        subcluster_length_list = np.zeros(len(subclusters))
+        for i_subcluster,subcluster in enumerate(subclusters):
+            joined = np.vstack([segments[ a_cluster[iii] ] for iii in subcluster])
+            fit_result = fit_rod(joined,linearity_threshold=0.0001,radius_curvature_threshold=100000)
+            subcluster_error_list[i_subcluster] = fit_result['err']
+            # rec = fit_result['rec']
+            subcluster_length_list[i_subcluster] = segments_length_list[ a_cluster[subcluster] ].sum()
+        
+        certification = (subcluster_error_list < subcluster_error_threshold) & (np.abs(subcluster_length_list - subcluster_length_threshold) < subcluster_length_tolerance)
+        
+        for i_cert in np.where(certification)[0]:
+            good_clusters.append([a_cluster[i] for i in subclusters[i_cert]])
+    print(f'Number of good clusters: ', len(good_clusters))
+    
+    good_nodes = np.hstack(good_clusters)
+    not_yet_nodes = np.setdiff1d(range(N_segments),good_nodes)
+    for i_ny in not_yet_nodes:
+        rr = segments[i_ny]
+        fit_result = fit_rod(rr,linearity_threshold=0.0001,radius_curvature_threshold=100000)
+        rr_len = np.sum(np.sqrt(np.sum(np.diff(rr,axis=0)**2,axis=1)))
+        
+        if fit_result['err'] < subcluster_error_threshold and (np.abs(rr_len - subcluster_length_threshold)<subcluster_length_tolerance):
+            good_clusters.append(np.array([i_ny]))
+            
+    good_nodes = np.hstack(good_clusters)
+    not_yet_nodes = np.setdiff1d(range(N_segments),good_nodes)
+
+    print(f'Number of good clusters: ', len(good_clusters))
+    print(f'Number of not yet nodes: ', len(not_yet_nodes))
+    print(f'Number of good nodes: ', len(good_nodes))
+    
+    return good_clusters,not_yet_nodes
+
+def sort_curve(rr):
+    centroid = np.mean(rr,axis=0)
+    rr_centered = rr - centroid        
+    _,_, V = np.linalg.svd(rr_centered, full_matrices=False)
+    v1 = V[0,:]
+    orientation = v1 * np.sign(np.sum(v1 * (rr_centered[-1, :] - rr_centered[0, :])))
+    slist = np.dot((rr - centroid), orientation)
+    sorted_indices = np.argsort(slist)
+    return centroid + rr_centered[sorted_indices]
+
+def subclustering_by_mst_length_lowerbound(clusters,segments,svd_cylinders,subcluster_error_threshold,subcluster_length_threshold):
+    N_segments = len(segments)    
+    segments_length_list = np.zeros(N_segments)
+    for i,seg in enumerate(segments):
+        segments_length_list[i] = np.sum(np.sqrt(np.sum(np.diff(seg,axis=0)**2,axis=1)))
+    cluster_length_list = np.zeros(len(clusters))
+    for i in range(len(clusters)):
+        cluster_length_list[i] = segments_length_list[clusters[i]].sum()
+    # np.argsort(cluster_length_list)[::-1]
+    local_trash_nodes = set()
+    good_clusters = []
+    for i_cluster, a_cluster in enumerate(clusters):
+        subclusters = explode_local_cluster(a_cluster,svd_cylinders,segments)
+        # check quality
+        subcluster_error_list = np.zeros(len(subclusters))
+        subcluster_length_list = np.zeros(len(subclusters))
+        for i_subcluster,subcluster in enumerate(subclusters):
+            joined = np.vstack([segments[ a_cluster[iii] ] for iii in subcluster])
+            joined = sort_curve(joined)
+            fit_result = fit_rod(joined,linearity_threshold=1e-10,radius_curvature_threshold=1e10)
+            subcluster_error_list[i_subcluster] = fit_result['err']
+            # rec = fit_result['rec']
+            # subcluster_length_list[i_subcluster] = segments_length_list[ a_cluster[subcluster] ].sum()
+            subcluster_length_list[i_subcluster] = np.sum(np.sqrt(np.sum(np.diff(joined,axis=0)**2,axis=1)))
+            
+        if (fit_result['planarity'] > 5):
+            # local_trash_nodes.update(subclusters[0][0])
+            for i_subcluster,subcluster in enumerate(subclusters):
+                if subcluster_length_list[i_subcluster] < 10:
+                    local_trash_nodes.add(subclusters[i_subcluster][0])
+        
+        if ((len(subcluster_error_list) == 1) and (subcluster_length_list[0] < 10)):
+            local_trash_nodes.add(subclusters[0][0])
+        
+        certification = (subcluster_error_list < subcluster_error_threshold) & (fit_result['planarity'] < 1) & (subcluster_length_list < 750)
+        
+        for i_cert in np.where(certification)[0]:
+            good_clusters.append(np.array([a_cluster[i] for i in subclusters[i_cert]]))
+            
+    print(f'[MST] Number of good clusters: ', len(good_clusters))
+    
+    # print(f'Number of good nodes: ', len(good_nodes))
+    
+    return good_clusters,local_trash_nodes
     
     
 if __name__ == '__main__':
