01f2d137623fb67348ab70baac63cf74e28febd7
Yeonsu Jung <ynsuju@gmail.com>
2024-06-01 05:03:39 -0400

    Update analyze_sim_dataset.py

--- analyze_sim_dataset.py
+++ analyze_sim_dataset.py
@@ -14,38 +14,10 @@ import filamentFields
 import argparse
 import datetime
 
+from visualizations import plot_contacts
+
 # mamba create -n simdata-analysis numpy scipy numba pandas matplotlib jax jaxlib
 # use python lower version
-
-def plot_contacts(contact_info,scale_factor,ax):
-    ni1 = contact_info["ni1"]
-    ni2 = contact_info["ni2"]
-    nj1 = contact_info["nj1"]
-    nj2 = contact_info["nj2"]
-    fi1 = contact_info["fi1"]
-    fi2 = contact_info["fi2"]
-    fj1 = contact_info["fj1"]
-    fj2 = contact_info["fj2"]
-    contact_point_i = contact_info["contact_point_i"]
-    contact_force_i = contact_info["contact_force_i"]
-    contact_point_j = contact_info["contact_point_j"]
-    contact_force_j = contact_info["contact_force_j"]
-    log_contact_force_i = contact_info["log_contact_force_i"]
-    log_contact_force_j = contact_info["log_contact_force_j"]
-    
-    if (np.isnan(contact_point_i).any() or np.isnan(contact_point_j).any()):
-        return
-    
-    ax.plot([ni1[0],ni2[0]],[ni1[1],ni2[1]],[ni1[2],ni2[2]],'r',linewidth=0.5)
-    ax.plot([nj1[0],nj2[0]],[nj1[1],nj2[1]],[nj1[2],nj2[2]],'r',linewidth=0.5)
-    ax.plot(contact_point_i[0],contact_point_i[1],contact_point_i[2],'g.')
-    ax.plot(contact_point_j[0],contact_point_j[1],contact_point_j[2],'g.')
-    
-    # log scale
-    ax.quiver(contact_point_i[0],contact_point_i[1],contact_point_i[2],log_contact_force_i[0]/scale_factor,log_contact_force_i[1]/scale_factor,log_contact_force_i[2]/scale_factor,color='g',linestyle='-')
-    ax.quiver(contact_point_j[0],contact_point_j[1],contact_point_j[2],log_contact_force_j[0]/scale_factor,log_contact_force_j[1]/scale_factor,log_contact_force_j[2]/scale_factor,color='g',linestyle='-')
-
-
 
 def guess_contact_point(fi1,fi2):
     fi1x = fi1[0]
@@ -67,14 +39,6 @@ def guess_contact_point(fi1,fi2):
     # assert(np.abs(x_frac-y_frac) < tol and np.abs(y_frac-z_frac) < tol)
     
     return x_frac
-
-
-# at edges between (idx1, idx3) and (idx2, idx4)
-# (4 * idx1 + e1, contact_gradient[e1], idx5);
-# (4 * idx3 + e1, contact_gradient[e1 + 3], idx5);
-# (4 * idx2 + e1, contact_gradient[e1 + 6], idx6);
-# (4 * idx4 + e1, contact_gradient[e1 + 9], idx6);
-
 def process_contact_data(single_contact_info,curr_nodes):
     # dismech originally computes force as a gradient of potential
     # thus here we need to change its sign
@@ -125,6 +89,18 @@ def process_contact_data(single_contact_info,curr_nodes):
                     "fj2":fj2}
     
     return contact_info
+def get_curr_force_essentials(curr_force_all_info,curr_nodes):
+            num_total_contacts = len(curr_force_all_info)
+            curr_force_essentials = np.zeros((num_total_contacts,6))
+            for query_index in range(num_total_contacts):
+                single_contact_info = curr_force_all_info[query_index]
+                contact_info = process_contact_data(single_contact_info,curr_nodes)        
+                pi = contact_info['contact_point_i']
+                pj = contact_info['contact_point_j']
+                cij = (pi+pj)/2
+                fij = contact_info['contact_force_i']        
+                curr_force_essentials[query_index] = np.array([cij[0],cij[1],cij[2],fij[0],fij[1],fij[2]])
+            return curr_force_essentials
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -139,8 +115,6 @@ def main():
         folder_path ='/Users/yeonsu/Data/from_cluster/20240528-1714_RUN_EntangleCarrotCake4,N1000_AR200_mu0.2_visc0_boxsize0.5_freq10_amp0.05'
     
     # python analyze_sim_dataset.py CarrotCake2-ExciteEntangle /Users/yeonsu/Data/from_cluster/20240528-1714_RUN_EntangleCarrotCake4,N1000_AR200_mu0.2_visc0_boxsize0.5_freq10_amp0.05
-    
-    
     print(f'Analyzing the dataset')
     print(f'Protocol ID: {protocol_id}')
     print(f'Folder path: {folder_path}')
@@ -151,13 +125,14 @@ def main():
     rod_length = 1
 
     mid_y = 0
-    num_grids = 50
+    num_grids = 30
     xlim = [-0.5,0.5]
+    ylim = [-0.5,0.5]
     zlim = [-1,1]
     
     visualize_fields = 1
     visualize_rods_contacts = 1
-    skip_frames = 1
+    skip_frames = 10
     max_rows = 100
     
     folder_path = Path(folder_path)
@@ -213,12 +188,13 @@ def main():
         print(f'Folder already exists: {output_folder}')
 
     current_datetime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    data_output_folder = f'{output_folder}/data_{current_datetime}/'
     field_output_folder = f'{output_folder}/fieldsPlots_{current_datetime}/'
-    contact_output_folder = f'{output_folder}/rodsContactPlots_{current_datetime}/'
-
+    contact_output_folder = f'{output_folder}/rodsContactPlots_{current_datetime}/'    
+    if not os.path.exists(data_output_folder):
+        os.makedirs(data_output_folder)
     if not os.path.exists(field_output_folder):
-        os.makedirs(field_output_folder)
-        
+        os.makedirs(field_output_folder)        
     if not os.path.exists(contact_output_folder):
         os.makedirs(contact_output_folder)
 
@@ -227,7 +203,8 @@ def main():
     print(f'Aspect ratio: {AR}')
     print(f'Output folder: {output_folder}')
 
-    mg = np.meshgrid(np.linspace(xlim[0],xlim[1],num_grids),mid_y,np.linspace(zlim[0],zlim[1],num_grids))
+    # mg = np.meshgrid(np.linspace(xlim[0],xlim[1],num_grids),mid_y,np.linspace(zlim[0],zlim[1],num_grids))
+    mg = np.meshgrid(np.linspace(xlim[0],xlim[1],num_grids),np.linspace(-ylim[0],ylim[1],num_grids),np.linspace(zlim[0],zlim[1],num_grids))
     sampling_points = np.array([mg[0].flatten(),mg[1].flatten(),mg[2].flatten()]).T
             
     n_fields_over_time = np.zeros((len(time_line),len(sampling_points)))
@@ -238,23 +215,12 @@ def main():
     f_fields_over_time = np.zeros((len(time_line),len(sampling_points)))
     total_entanglement_over_time = np.zeros(len(time_line))
     
-    fF = filamentFields.filamentFields([],[])
-    
+    fF = filamentFields.filamentFields([],[])    
     start = time.time()
-    for frame in range(0,len(time_line),skip_frames):
+    for frame in range(0,len(time_line),1):
         curr_nodes = node_list[frame].reshape((-1,10,3))
         curr_force_all_info = contact_list[frame].reshape(-1,18)
-        num_total_contacts = len(curr_force_all_info)
-
-        curr_force_essentials = np.zeros((num_total_contacts,6))
-        for query_index in range(num_total_contacts):
-            single_contact_info = curr_force_all_info[query_index]
-            contact_info = process_contact_data(single_contact_info,curr_nodes)        
-            pi = contact_info['contact_point_i']
-            pj = contact_info['contact_point_j']
-            cij = (pi+pj)/2
-            fij = contact_info['contact_force_i']        
-            curr_force_essentials[query_index] = np.array([cij[0],cij[1],cij[2],fij[0],fij[1],fij[2]])
+        curr_force_essentials = get_curr_force_essentials(curr_force_all_info,curr_nodes)
             
         if visualize_rods_contacts and (frame % skip_frames == 0):
             fig,ax=plt.subplots(1,1,figsize=(10,10),subplot_kw={'projection':'3d'})
@@ -280,21 +246,28 @@ def main():
         c_fields = np.zeros(len(sampling_points))
         f_fields = np.zeros(len(sampling_points))
         
-        fF.updateFilamentNodesList(curr_nodes)
-        fF.updateContactArray(curr_force_essentials)
+        fF.update_filament_nodes_list(curr_nodes)
+        fF.update_contact_array(curr_force_essentials)
         
         another_start = time.time()
-        fF.compute_total_linking_matrix()
-        print(f'Elapsed time for computing total linking matrix: {time.time()-another_start}')
+        fF.precompute(R_omega)
+        print(f'Elapsed time for precompute: {time.time()-another_start}')
+        
+        another_start = time.time()
+        
+        
+        # result = fF.analyze_local_volume_over_domain(sampling_points, R_omega, rod_diameter)
+        # result.shape
         
         for iterator,sampling_point in enumerate(sampling_points):
-            fF.analyzeLocalVolumeFromPrecomputed(sampling_point, R_omega, rod_diameter)
+            fF.analyze_local_volume_from_precomputed(sampling_point, R_omega, rod_diameter)
             n_fields[iterator] = fF.return_number_of_labels()
             phi_fields[iterator] = fF.return_volume_fraction()
             S_fields[iterator] = fF.return_orientational_order_parameter()
             e_fields[iterator] = fF.return_entanglement()
             c_fields[iterator] = fF.return_number_of_local_contacts()
             f_fields[iterator] = fF.return_force_sum()
+        print(f'Elapsed time for local sampling and adding: {time.time()-another_start}')        
         
         n_fields_over_time[frame] = n_fields
         phi_fields_over_time[frame] = phi_fields
@@ -302,30 +275,30 @@ def main():
         e_fields_over_time[frame] = e_fields
         c_fields_over_time[frame] = c_fields
         f_fields_over_time[frame] = f_fields
-        
-        
         total_entanglement_over_time[frame] = fF.return_total_entanglement()
         
+        
         if visualize_fields and (frame % skip_frames == 0):
-            phi_image = phi_fields.reshape((num_grids,num_grids))
-            phi_image = np.flipud(phi_image.T)
-            S_image = S_fields.reshape((num_grids,num_grids))
+            n_image = np.max(n_fields.reshape((num_grids,num_grids,num_grids)),axis=0)
+            S_image = np.max(S_fields.reshape((num_grids,num_grids,num_grids)),axis=0)            
+            e_image = np.max(e_fields.reshape((num_grids,num_grids,num_grids)),axis=0)
+            c_image = np.max(c_fields.reshape((num_grids,num_grids,num_grids)),axis=0)
+            f_image = np.max(f_fields.reshape((num_grids,num_grids,num_grids)),axis=0)
+            
+            n_image = np.flipud(n_image.T)
             S_image = np.flipud(S_image.T)
-            e_image = e_fields.reshape((num_grids,num_grids))
             e_image = np.flipud(e_image.T)
-            c_image = c_fields.reshape((num_grids,num_grids))
             c_image = np.flipud(c_image.T)
-            f_image = f_fields.reshape((num_grids,num_grids))
-            f_image = np.flipud(f_image.T)            
+            f_image = np.flipud(f_image.T)
         
             fig,axs=plt.subplots(1,5,figsize=(20,4))
-            fig.colorbar(axs[0].imshow(phi_image, extent=[xlim[0], xlim[1], zlim[0], zlim[1]]), ax=axs[0])
+            fig.colorbar(axs[0].imshow(n_image, extent=[xlim[0], xlim[1], zlim[0], zlim[1]]), ax=axs[0])
             fig.colorbar(axs[1].imshow(S_image, extent=[xlim[0], xlim[1], zlim[0], zlim[1]]), ax=axs[1])
             fig.colorbar(axs[2].imshow(e_image, extent=[xlim[0], xlim[1], zlim[0], zlim[1]]), ax=axs[2])
             fig.colorbar(axs[3].imshow(c_image, extent=[xlim[0], xlim[1], zlim[0], zlim[1]]), ax=axs[3])
             fig.colorbar(axs[4].imshow(f_image, extent=[xlim[0], xlim[1], zlim[0], zlim[1]]), ax=axs[4])
             
-            axs[0].set_title('Volume fraction')
+            axs[0].set_title('Number of local rods')
             axs[1].set_title('Orientational order parameter')
             axs[2].set_title('Entanglement')
             axs[3].set_title('Number of contacts')
@@ -336,11 +309,9 @@ def main():
             plt.savefig(f'{field_output_folder}/frame_{frame:04d}.png',dpi=300)
             plt.close()
         
-        if (frame % 10 == 0):
+        if (frame % 1 == 0):
             print(f'Evaluated {frame} frames so far. Elapsed time: {time.time()-start:.2f} sec')
-            
-    
-    np.savez_compressed(f'{output_folder}/all_fields_over_time.npz',
+            np.savez_compressed(f'{data_output_folder}/all_fields_over_time.npz',
                         n_fields_over_time=n_fields_over_time,
                         phi_fields_over_time=phi_fields_over_time,
                         S_fields_over_time=S_fields_over_time,
