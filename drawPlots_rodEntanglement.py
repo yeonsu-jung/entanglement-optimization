37c7c0e2b9bc0521feaf3847fc51e9cbb50a2eb7
Yeonsu Jung <ynsuju@gmail.com>
2024-06-10 14:25:49 -0400

    Update drawPlots_rodEntanglement.py

--- drawPlots_rodEntanglement.py
+++ drawPlots_rodEntanglement.py
@@ -20,6 +20,8 @@ pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/analysis-data/E
 pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/analysis-data/EatEntangledCarrotCake5/NonIntersectingBox-N375-AR75-Scale1_20240531-222436')
 pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/analysis-data/EatEntangledCarrotCake5/NonIntersectingBox-N500-AR100-Scale1_20240531-222436')
 pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/analysis-data/EatEntangledCarrotCake5/NonIntersectingBox-N625-AR125-Scale1_20240531-222434')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/analysis-data/EatEntangledCarrotCake5/NonIntersectingBox-N1000-AR200-Scale1_20240603-131308')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/analysis-data/EatEntangledCarrotCake5/NonIntersectingBox-N1500-AR300-Scale1_20240603-000746')
 num_samples = len(pathlist)
 # %%
 data_path_list = []
@@ -45,7 +47,8 @@ if not os.path.exists(output_path):
 # %%
 figure_handles = []
 ax_handles = []
-for _i in range(6):
+num_field_types = 5
+for _i in range(num_field_types):
     fig,axs=plt.subplots(1,3,figsize=(15,5))
     figure_handles.append(fig)
     ax_handles.append(axs)
@@ -58,11 +61,11 @@ for dta in data_path_list:
     total_entanglement_over_time = all_fields['total_entanglement_over_time']
         
     field_list = []
-    field_list.append(all_fields['n_fields_over_time'])
+    # field_list.append(all_fields['n_fields_over_time'])
     field_list.append(all_fields['phi_fields_over_time'])
     field_list.append(all_fields['S_fields_over_time'])
-    field_list.append(all_fields['e_fields_over_time'])
     field_list.append(all_fields['c_fields_over_time'])
+    field_list.append(all_fields['e_fields_over_time'])
     field_list.append(all_fields['f_fields_over_time'])
     # field_list.append(all_fields['Q_fields_over_time'])
     
@@ -87,45 +90,36 @@ for dta in data_path_list:
         local_ax[2].plot(time_line,coeff_var_field)
         local_ax[0].set(xlabel='time (s)', ylabel='Mean')
         local_ax[1].set(xlabel='time (s)', ylabel='Std')
-        local_ax[2].set(xlabel='time (s)', ylabel='Coeff. Var.')
+        local_ax[2].set(xlabel='time (s)', ylabel='Coeff. Var.')        
+        
+        if _ifield == 2 or _ifield == 4:
+            local_ax[2].set_ylim([0,3])
 
-titles = ['N','phi','S','e','c','f']
-for _i in range(6):
+titles = ['Volume fraction','Nematic order','Contact','Entanglement','Contact force']
+
+for _i in range(num_field_types):
     local_ax = ax_handles[_i]
-    local_ax[1].set_title(titles[_i])
+    local_ax[1].set_title(titles[_i],fontsize=16,fontweight='bold')
     
 # ax_handles[0][2].legend([f'{_i}' for _i in range(5)])
 # label
-for _i in range(6):
-    ax_handles[_i][2].legend([f'N{_i}' for _i in range(num_samples)])
+num_rods_items = [125,250,375,500,625,1000,1500]
+AR_items = [25,50,75,100,125,200,300]
+for _i in range(num_field_types):
+    ax_handles[_i][2].legend([f'AR{AR_items[_i]}' for _i in range(num_samples)])
     
+    
+panel_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R']
+
+# Add panel labels
+for _i in range(num_field_types):
+    for _j in range(3):
+        pannel_letter = panel_letters[_i*3+_j]
+        ax_handles[_i][_j].annotate(pannel_letter, xy=(0, 1.05), xycoords='axes fraction',
+                                   ha='center', va='center', fontsize=12, fontweight='bold')
+
+
 plt.legend()
-# %%
-len(ax_handles)
-    
-# %%
-axs[0,0].set_title('Total Entanglement')
-axs[0,1].set_title('Mean')
-axs[1,0].set_title('Std')
-axs[1,1].set_title('Coeff. Var.')
-# %%
-
-for ax in axs.flat:
-    ax.set(xlabel='time (s)', ylabel='a.u.')
-    ax.label_outer()
-
-
-# %%
-phi_fields_over_time = all_fields['phi_fields_over_time']
-
-mean_field = np.zeros(len(time_line))
-for _i in range(len(time_line)):
-    mean_field[_i] = np.nanmean(phi_fields_over_time[_i])
-    
-plt.plot(time_line,mean_field)
-
-
-
-# for _time in [0,1,2,3,4,5,6,7,8,9,10]:
-#     ax.axvline(x=_time, color='k', linestyle='--', alpha=0.5)
-
+for _i in range(num_field_types):
+    fig = figure_handles[_i]
+    fig.savefig(f'{output_path}/fields_{titles[_i]}.png',dpi=300)
