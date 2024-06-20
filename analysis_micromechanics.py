dd0805458b766b676da1298d418a46ac43d32843
Yeonsu Jung <ynsuju@gmail.com>
2024-06-20 19:02:04 -0400

    Update analysis_micromechanics.py

--- analysis_micromechanics.py
+++ analysis_micromechanics.py
@@ -59,7 +59,7 @@ def analyze_a_path(pth):
     # correlation with the initial data!
     initial_nodes = node_list[0].reshape((-1,10,3))
     
-    fF = filamentFields.filamentFields([],[])
+    # fF = filamentFields.filamentFields([],[])
 
     for frame in range(0,len(time_line)-1,1):
         curr_nodes = node_list[frame].reshape((-1,10,3))
@@ -116,7 +116,7 @@ def analyze_a_path(pth):
 output_root = '/Users/yeonsu/Dropbox (Harvard University)/Data/PrunedData/rod-sim-pnas-revision'
 
 # Entangle
-protocol_id = 'HangModelos'
+protocol_id = 'Micromechanics-HeavyHangModelos'
 
 output_path = f'{output_root}/{protocol_id}'
 if not os.path.exists(output_path):
@@ -124,7 +124,7 @@ if not os.path.exists(output_path):
 
 # %%
 parent_folders = []
-parent_folders.append(Path('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/HangModelo1'))
+parent_folders.append(Path('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/HeavyHangModelo1'))
 # parent_folders.append(Path('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/Modelo3_FineExcitation'))
 # parent_folders.append(Path('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/Modelo2_FineExcitation'))
 # pathlist is subdirs, not including itself
@@ -196,15 +196,15 @@ for parent_folder in parent_folders:
         last_frame = len(time_line)-1
         print(f'Last frame: {last_frame}')
         
-        initial_nodes = node_list[0].reshape((-1,10,3))        
+        initial_nodes = node_list[0].reshape((-1,10,3))
         timeline_checkout = range(0,len(time_line)-1,skip_frames)
         avg_velocities_over_time = np.zeros( len(timeline_checkout) )
         centroid_velocities_over_time = np.zeros( len(timeline_checkout) )
         avg_contact_displacement_over_time = np.zeros( len(timeline_checkout) )
-        avg_initial_centroid_displacement_over_time = np.zeros( len(timeline_checkout) )        
+        avg_initial_centroid_displacement_over_time = np.zeros( len(timeline_checkout) )
         total_entanglement_over_time = np.zeros( len(timeline_checkout) )
         
-        fF = filamentFields.filamentFields([],[])
+        # fF = filamentFields.filamentFields([],[])
         
         for i_frame,frame in enumerate(timeline_checkout):
             
@@ -225,23 +225,21 @@ for parent_folder in parent_folders:
             graph.add_nodes_from(range(len(curr_nodes)))
             graph.add_edges_from(contact_ij)
             
-            graph[0]
+            
     
-            fF.update_filament_nodes_list(curr_nodes)
-            R_omega = 10000
-            fF.compute_total_linking_matrix()
-            lk_mat = fF.return_total_linking_matrix()
+            # fF.update_filament_nodes_list(curr_nodes)
+            # R_omega = 10000
+            # fF.compute_total_linking_matrix()
+            # lk_mat = fF.return_total_linking_matrix()
             
-            lk_mat[0,15]
+            # total_entanglement_over_time[i_frame] = fF.return_total_entanglement()
             
-            total_entanglement_over_time[i_frame] = fF.return_total_entanglement()
-            
-            for i_,contact_entry in enumerate(curr_force_all_info):
-                popt_i,popt_j,dvec,x_i1,x_i2,x_j1,x_j2 = get_closest_points(contact_entry,curr_nodes)
-                popt_i_next,popt_j_next,dvec_next,x_i1_next,x_i2_next,x_j1_next,x_j2_next = get_closest_points(contact_entry,next_nodes)
-                contact_displacement = dvec_next - dvec
-                contact_displacement_norm = np.linalg.norm(contact_displacement)
-                contact_displacement_list[i_] = contact_displacement_norm
+            # for i_,contact_entry in enumerate(curr_force_all_info):
+            #     popt_i,popt_j,dvec,x_i1,x_i2,x_j1,x_j2 = get_closest_points(contact_entry,curr_nodes)
+            #     popt_i_next,popt_j_next,dvec_next,x_i1_next,x_i2_next,x_j1_next,x_j2_next = get_closest_points(contact_entry,next_nodes)
+            #     contact_displacement = dvec_next - dvec
+            #     contact_displacement_norm = np.linalg.norm(contact_displacement)
+            #     contact_displacement_list[i_] = contact_displacement_norm
                 
             average_contact_displacement = np.mean(contact_displacement_list)
             avg_contact_displacement_over_time[i_frame] = average_contact_displacement
@@ -373,4 +371,147 @@ for _i in range(num_rods):
     for _j in range(num_rods):
         rod_j = curr_nodes[_j]
         
+# %%
+# pathlist = []
+
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240609-1051_RUN_TickleEEModelo1_N0125_AR025_a100')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240609-1052_RUN_TickleEEModelo1_N0500_AR100_a100')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240609-1052_RUN_TickleEEModelo1_N1500_AR300_a100')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1743_RUN_TickleModelo1_N0500_AR100_a02')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1743_RUN_TickleModelo1_N0500_AR100_a05')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1743_RUN_TickleModelo1_N0500_AR100_a10')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1743_RUN_TickleModelo1_N0500_AR100_a25')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1743_RUN_TickleModelo1_N0500_AR100_a50')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1746_RUN_TickleModelo1_N0125_AR025_a02')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1746_RUN_TickleModelo1_N0125_AR025_a05')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1746_RUN_TickleModelo1_N0125_AR025_a10')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1746_RUN_TickleModelo1_N0125_AR025_a25')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/StudyPhaseDiagram/20240613-1746_RUN_TickleModelo1_N0125_AR025_a50')
+
+# # %%
+# avg_velocities_wrt_AR_repeated = []
+# avg_centroid_velocity_wrt_AR_repeated = []
+# avg_contact_displacement_wrt_AR_repeated = []
+# avg_initial_centroid_displacement_wrt_AR_repeated = []
+# initial_centroid_disp_over_time_wrt_AR_repeated = []
+# total_entanglement_over_time_wrt_AR_repeated = []
+
+# skip_frames = 5
+# for pth in pathlist:
+#     # if os.path.exists(f'{output_path}/avg_contact_displacement_wrt_AR.pkl'):
+#     #     exit()
+
+#     # if not os.path.exists(f'{output_path}/avg_contact_displacement_wrt_AR.pkl'):
+#     #     pass
+    
+#     # find csv file
+#     data_path = None
+#     for file in Path(pth).rglob('*.csv'):
+#         if str(file.stem).endswith('lastFrame'):
+#             continue        
+#         data_path = file
+#         break
+    
+#     log_string = ''
+    
+#     file_id,surfix,num_rods,AR,datetime_string = parse_path_string(data_path)
+#     time_line, node_list, contact_list = import_all_log(data_path,max_rows=100000)
+
+#     time_line = np.array(time_line)
+#     time_line = time_line[time_line <= 10]
+#     node_list = node_list[:len(time_line)]
+#     contact_list = contact_list[:len(time_line)]
+
+#     time_line = time_line[1:]
+#     node_list = node_list[1:]
+#     contact_list = contact_list[1:]
+
+#     print(f'Size of time_line: {len(time_line)}')
+#     print(f'Number of rods: {num_rods}')
+
+#     log_string = log_string + f'Number of rods: {num_rods}\n'
+#     log_string = log_string + f'Number of time points: {len(time_line)}\n'
+
+#     total_number_of_contacts = np.zeros(len(time_line))
+#     total_force_sum = np.zeros(len(time_line))
+
+#     last_frame = len(time_line)-1
+#     print(f'Last frame: {last_frame}')
+    
+#     initial_nodes = node_list[0].reshape((-1,10,3))        
+#     timeline_checkout = range(0,len(time_line)-1,skip_frames)
+#     avg_velocities_over_time = np.zeros( len(timeline_checkout) )
+#     centroid_velocities_over_time = np.zeros( len(timeline_checkout) )
+#     avg_contact_displacement_over_time = np.zeros( len(timeline_checkout) )
+#     avg_initial_centroid_displacement_over_time = np.zeros( len(timeline_checkout) )        
+#     total_entanglement_over_time = np.zeros( len(timeline_checkout) )
+    
+#     fF = filamentFields.filamentFields([],[])
+    
+#     for i_frame,frame in enumerate(timeline_checkout):
         
+#         curr_nodes = node_list[frame].reshape((-1,10,3))
+#         next_nodes = node_list[frame+1].reshape((-1,10,3))
+#         curr_force_all_info = contact_list[frame].reshape(-1,18)
+#         next_force_all_info = contact_list[frame+1].reshape(-1,18)
+        
+#         initial_node_displacement = curr_nodes - initial_nodes
+#         avg_initial_centroid_displacement =  np.mean(np.linalg.norm(np.mean(initial_node_displacement,axis=1),axis=1))
+#         avg_initial_centroid_displacement_over_time[i_frame] = avg_initial_centroid_displacement
+        
+#         contact_displacement_list = np.zeros(len(curr_force_all_info))
+        
+#         contact_ij = curr_force_all_info[:,4:6].astype(int)
+#         contact_ij_next_frame = next_force_all_info[:,4:6].astype(int)            
+#         graph = nx.Graph()
+#         graph.add_nodes_from(range(len(curr_nodes)))
+#         graph.add_edges_from(contact_ij)
+        
+#         graph[0]
+
+#         fF.update_filament_nodes_list(curr_nodes)
+#         R_omega = 10000
+#         fF.compute_total_linking_matrix()
+#         lk_mat = fF.return_total_linking_matrix()
+        
+#         lk_mat[0,15]
+        
+#         total_entanglement_over_time[i_frame] = fF.return_total_entanglement()
+        
+#         for i_,contact_entry in enumerate(curr_force_all_info):
+#             popt_i,popt_j,dvec,x_i1,x_i2,x_j1,x_j2 = get_closest_points(contact_entry,curr_nodes)
+#             popt_i_next,popt_j_next,dvec_next,x_i1_next,x_i2_next,x_j1_next,x_j2_next = get_closest_points(contact_entry,next_nodes)
+#             contact_displacement = dvec_next - dvec
+#             contact_displacement_norm = np.linalg.norm(contact_displacement)
+#             contact_displacement_list[i_] = contact_displacement_norm
+            
+#         average_contact_displacement = np.mean(contact_displacement_list)
+#         avg_contact_displacement_over_time[i_frame] = average_contact_displacement
+        
+#         # node velocities
+#         rod_velocities = np.zeros((num_rods,10,3))
+#         for i_rod in range(0,num_rods,1):
+#             curr_rod = curr_nodes[i_rod]
+#             next_rod = next_nodes[i_rod]
+#             rod_velocities[i_rod] = next_rod - curr_rod
+            
+#         avg_velocity_at_the_frame = np.mean(np.linalg.norm(rod_velocities.reshape(-1,3),axis=1))
+#         avg_velocities_over_time[i_frame] = avg_velocity_at_the_frame
+        
+#         centroid_velocities_at_the_frame = np.mean(rod_velocities,axis=1)
+#         centroid_velocities_over_time[i_frame] = np.mean(np.linalg.norm(centroid_velocities_at_the_frame,axis=1))
+        
+#     avg_velocities_wrt_AR.append(np.mean(avg_velocities_over_time*0.05/1e-5/100))
+#     avg_centroid_velocity_wrt_AR.append(np.mean(centroid_velocities_over_time*0.05/1e-5/100))
+#     avg_contact_displacement_wrt_AR.append(np.mean(avg_contact_displacement_over_time*0.05/1e-5/100))
+#     avg_initial_centroid_displacement_wrt_AR.append(np.mean(avg_initial_centroid_displacement_over_time*0.05/1e-5/100))
+#     initial_centroid_disp_over_time_wrt_AR.append(avg_initial_centroid_displacement_over_time*0.05/1e-5/100)        
+#     total_entanglement_over_time_wrt_AR.append(total_entanglement_over_time)
+
+# avg_velocities_wrt_AR_repeated.append(avg_velocities_wrt_AR)
+# avg_centroid_velocity_wrt_AR_repeated.append(avg_centroid_velocity_wrt_AR)
+# avg_contact_displacement_wrt_AR_repeated.append(avg_contact_displacement_wrt_AR)
+# avg_initial_centroid_displacement_wrt_AR_repeated.append(avg_initial_centroid_displacement_wrt_AR)
+# initial_centroid_disp_over_time_wrt_AR_repeated.append(initial_centroid_disp_over_time_wrt_AR)
+# total_entanglement_over_time_wrt_AR_repeated.append(total_entanglement_over_time_wrt_AR)
+# # %%
