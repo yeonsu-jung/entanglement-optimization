3ec3a384c65ce2c9b72920026466d86e15e771d1
Yeonsu Jung <ynsuju@gmail.com>
2024-06-20 19:03:22 -0400

    Update example_exportLastFrames.py

--- example_exportLastFrames.py
+++ example_exportLastFrames.py
@@ -9,14 +9,30 @@ from pathlib import Path
 
 
 
-last_frame_out_path = f'/Users/yeonsu/GitHub/dismech-rods-main/data/EntangledModelo3/'
+last_frame_out_path = f'/Users/yeonsu/GitHub/dismech-rods-main/data/CalmModelo1/'
 
 pathlist = []
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N1500_AR300')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N1250_AR250')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N1000_AR200')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0875_AR175')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0750_AR150')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0625_AR125')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0600_AR120')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0575_AR115')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0550_AR110')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0525_AR105')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0500_AR100')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0450_AR090')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0400_AR080')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0375_AR075')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0350_AR070')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0300_AR060')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240620-0039_RUN_CalmEEModelo1_N0250_AR050')
+pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/20240619-2353_RUN_CalmEEModelo1_N0125_AR025')
 
-pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/EntangledModelo3/20240610-2258_RUN_EntangleModelo3_N0300_AR060')
-pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/EntangledModelo3/20240610-2258_RUN_EntangleModelo3_N0350_AR070')
-pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/EntangledModelo3/20240610-2258_RUN_EntangleModelo3_N0400_AR080')
-pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/EntangledModelo3/20240610-2258_RUN_EntangleModelo3_N0450_AR090')
+# pathlist.append('/Users/yeonsu/Dropbox (Harvard University)/Data/from-cluster/CalmModelo1/20240619-2353_RUN_CalmEEModelo1_N0125_AR025')
+
 
 
 for folder_path in pathlist:
